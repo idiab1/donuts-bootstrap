62e37fa1a59242b12fbb4f7245b5252fb137fdeb
islamdiab-stack <islamdiab967@gmail.com>
2021-09-11 12:04:24 +0200

    add class scroll in navbar when scroll y greater then 500 and remove in else

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,12 @@ const navbar = document.querySelector(".navbar");
 // Adjust height full screen for header
 document.querySelector("body").style.marginTop = navbar.clientHeight + "px";
 header.style.height = window.innerHeight - navbar.clientHeight + "px";
+
+// Adjust add class when scrolled
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 500) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});

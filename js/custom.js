a46d7d44a94cc99c21315260685fde7fda617b1e
islamdiab-stack <islamdiab967@gmail.com>
2021-09-14 11:55:56 +0200

    Create function for show and hide button top and create function for scroll to store section

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,44 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+// Scroll to store section when click on explore button
+// Select on explore button
+const exploreBtn = document.getElementById("explore-btn");
+const exploreAboutBtn = document.getElementById("explore-about-btn");
+const storeSection = document.getElementById("store");
+
+function scrolling(element) {
+  window.scrollTo({
+    behavior: "smooth",
+    left: 0,
+    top: element.offsetTop - 30,
+  });
+}
+exploreBtn.addEventListener("click", () => {
+  scrolling(storeSection);
+});
+exploreAboutBtn.addEventListener("click", () => {
+  scrolling(storeSection);
+});
+
+// Show and hide button when scrolling
+// Select on button top
+const buttonTop = document.querySelector(".button-top");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 1000) {
+    buttonTop.style.display = "block";
+  } else {
+    buttonTop.style.display = "none";
+  }
+});
+
+// Adjust scroll to top when click on button width smoothly
+buttonTop.addEventListener("click", () => {
+  window.scrollTo({
+    behavior: "smooth",
+    left: 0,
+    top: 0,
+  });
+});
